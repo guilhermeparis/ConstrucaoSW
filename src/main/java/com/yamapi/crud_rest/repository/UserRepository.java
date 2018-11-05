c81d1db7e7cca3701f7f912ba7e1f9540d857586
root <root@ages-VirtualBox>
2018-11-05 19:09:46 -0200

    Adicionada validação de acesso

--- src/main/java/com/yamapi/crud_rest/repository/UserRepository.java
+++ src/main/java/com/yamapi/crud_rest/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.yamapi.crud_rest.repository;
 import com.yamapi.crud_rest.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(path = "/users")
+@RestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, String> {
 
 }
